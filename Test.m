3956d46ba00a0b182da394afac539c7728f765e6
fa28 <fa28@st-andrews.ac.uk>
2015-09-25 14:57:03 +0100

    Created test script sine wave

---
+++ Test.m
@@ -0,0 +1,11 @@
+%  testSandbox
+% Use this repository for mucking around and testing git
+h = animatedline;
+axis([0,4*pi,-1,1])
+
+x = linspace(0,4*pi,1000);
+y = sin(x);
+for k = 1:length(x)
+    addpoints(h,x(k),y(k));
+    drawnow
+end
