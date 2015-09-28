1fa856486e75994d602ba133b23c9a8e67478c01
fa28 <fa28@st-andrews.ac.uk>
2015-09-28 16:17:37 +0100

    basic pitch code
    able to change pitch and duration but not sample number? ask justin at
    some point

---
+++ basic_pitch_code.m
@@ -0,0 +1,8 @@
+cf = 2000;                  % carrier frequency (Hz)
+sf = 22050;                 % sample frequency (Hz)
+d = 1.0;                    % duration (s)
+n = sf * d;                 % number of samples
+s = (1:n) / sf;             % sound data preparation
+s = sin(2 * pi * cf * s);   % sinusoidal modulation
+sound(s, sf);               % sound presentation
+pause(d + 0.5);             % waiting for sound end
