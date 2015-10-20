775339fc7792ad9d2d1a4a11107b614ee535f02f
fa28 <fa28@st-andrews.ac.uk>
2015-10-20 14:20:45 +0100

    guess_my_number-loop function practice
    edited to include else if

--- guess_my_number.m
+++ guess_my_number.m
@@ -1,4 +1,8 @@
 function guess_my_number(x)
-if x == 2
+if x == 42
     fprintf('congrats you guessed my number!\n')
+elseif x < 42
+    fprintf('too small, try again\n')
+else
+    fprintf ('not quite you wolly guess again')
 end
