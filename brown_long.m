80878974533c7df9aad789770af24de201d86c7b
fa28 <fa28@st-andrews.ac.uk>
2016-01-11 14:25:29 +0000

    brown_motion
    tinkering with values

--- brown_long.m
+++ brown_long.m
@@ -1,8 +1,8 @@
-nt = 100 %number of data points
+nt = 10 %number of data points
 obs_noise=0.5%obervation noise
-gain=0.355 % kalman gain conifidence in the previous measure
-X(1) = 0.5 % starting posistion
-P (1) = 1 %??
+gain=0.390 % kalman gain conifidence in the previous measure
+X(1) = 0.3 % starting posistion
+P (1) = 1 %measurment update?
 obs(1)=X(1)+randn()*obs_noise;%starting point for first observation + noise
 estimate(1)=obs (1)
 for it=2:nt
