7e4377f32a24d4048442c56d616ddb2f2dc8a725
fa28 <fa28@st-andrews.ac.uk>
2016-01-11 13:50:58 +0000

    brownin_motion

---
+++ brown_long.m
@@ -0,0 +1,22 @@
+nt = 100 %number of data points
+obs_noise=0.5%obervation noise
+gain=0.355 % kalman gain conifidence in the previous measure
+X(1) = 0.5 % starting posistion
+P (1) = 1 %??
+obs(1)=X(1)+randn()*obs_noise;%starting point for first observation + noise
+estimate(1)=obs (1)
+for it=2:nt
+    X(it)=X(it-1)+randn(); % line in which model values are outlined
+    obs(it) =X(it)+randn()*obs_noise;
+    error=estimate(it-1)-obs(it)
+    estimate(it)=estimate(it-1)-error*gain;
+end
+
+clf;
+
+plot(X)
+hold on
+plot (obs)
+plot (estimate, 'g')
+
+
