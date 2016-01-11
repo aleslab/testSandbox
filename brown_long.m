87e81215e3c69fd43547a754aa5582e3e76feeda
fa28 <fa28@st-andrews.ac.uk>
2016-01-11 14:33:10 +0000

    brown_long
    values

--- brown_long.m
+++ brown_long.m
@@ -1,10 +1,10 @@
 nt = 10 %number of data points
-obs_noise=0.5%obervation noise
-gain=0.390 % kalman gain conifidence in the previous measure
-X(1) = 0.3 % starting posistion
-P (1) = 1 %measurment update?
+obs_noise=0.3%obervation noise
+gain=0.2 % kalman gain conifidence in the previous measure
+X(1) = 0.8 % starting posistion
+P (1) = 0.4 %measurment update?
 obs(1)=X(1)+randn()*obs_noise;%starting point for first observation + noise
-estimate(1)=obs (1)
+estimate(1)=obs (1) %estimate of obs
 for it=2:nt
     X(it)=X(it-1)+randn(); % line in which model values are outlined
     obs(it) =X(it)+randn()*obs_noise;
